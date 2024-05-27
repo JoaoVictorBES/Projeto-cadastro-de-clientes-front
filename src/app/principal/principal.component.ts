a17c97e677ed77e125e3d40b20f2504ab40557f2
Joao Victor <victorsrodrigues99@gmail.com>
2024-05-27 20:06:30 -0300

    Configurando visibilidade dos botões

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+
+  //Variável para visibilade dos botões
+  btnCadastro:boolean = true;
 
 }
